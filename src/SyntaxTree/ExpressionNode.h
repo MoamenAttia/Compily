1c7000726a6ed393926dc391f57e06a2d80c53fa
Moamen Hassan <moamenattia@outlook.com>
2020-04-25 22:49:03 +0200

    expression node

--- src/SyntaxTree/ExpressionNode.h
+++ src/SyntaxTree/ExpressionNode.h
@@ -6,33 +6,75 @@
 
 class ExpressionNode : public Node
 {
+
+protected:
     BinaryOperationNode *binaryOperationNode;
     UnaryOperationNode *unaryOperationNode;
+    ExpressionNode *expressionNode;
     ExpressionNodeEnum expType;
-    ExpressionNode* expressionNode;
-    Value val;
+    string identifier;
+
+    int intVal;
+    bool boolVal;
+    float floatVal;
+    string stringVal;
 
 public:
-    ExpressionNode(Node *node, ExpressionNodeEnum expType, DataType dataType, Value val)
+    ExpressionNode() {}
+
+    // Expression constructor for binary operation node.
+    ExpressionNode(BinaryOperationNode *binaryOperationNode)
     {
-        this->expType = expType;
-        switch (expType)
-        {
-        case BINARY_OPERATION:
-            this->binaryOperationNode = (BinaryOperationNode*) node;
-            break;
-        case UNARY_OPERATION:
-            this->unaryOperationNode = (UnaryOperationNode*) node;
-            break;
-        case RECURSIVE_EXPRESSION:
-            this->expressionNode = expressionNode;
-            break;
-        case EXPRESSION_VALUE:
-            this->val = val;
-            break;
-        default:
-            break;
-        }
+        this->binaryOperationNode = binaryOperationNode;
+        this->expType = EXPRESSION_BINARY_OPERATION;
+    }
+
+    // Expression constructor for unary operation node.
+    ExpressionNode(UnaryOperationNode *unaryOperationNode)
+    {
+        this->unaryOperationNode = unaryOperationNode;
+        this->expType = EXPRESSION_UNARY_OPERATION;
+    }
+
+    // Expression constructor for braces expression
+    ExpressionNode(ExpressionNode *expressionNode)
+    {
+        this->expressionNode = expressionNode;
+        this->expType = EXPRESSION_BRACES_EXPRESSION;
+    }
+
+    // Expression constructor for integer value
+    ExpressionNode(int intVal)
+    {
+        this->intVal = intVal;
+        this->expType = EXPRESSION_INT_VALUE;
+    }
+
+    // Expression constructor for float value
+    ExpressionNode(float floatVal)
+    {
+        this->floatVal = floatVal;
+        this->expType = EXPRESSION_FLOAT_VALUE;
+    }
+
+    // Expression constructor for boolean value
+    ExpressionNode(bool boolVal)
+    {
+        this->boolVal = boolVal;
+        this->expType = EXPRESSION_BOOL_VALUE;
+    }
+
+    // Expression constructor for string value
+    ExpressionNode(string stringVal)
+    {
+        this->stringVal = stringVal;
+        this->expType = EXPRESSION_STRING_VALUE;
+    }
+
+    ExpressionNode(string identifier, bool isIdentifier)
+    {
+        this->identifier = identifier;
+        this->expType = EXPRESSION_IDENTIFIER;
     }
 
     ~ExpressionNode();
