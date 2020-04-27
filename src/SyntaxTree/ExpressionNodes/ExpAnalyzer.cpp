f8195c6db55288c4a4f2bb33d4261fcd5b01b8ba
Moamen Hassan <moamenattia@outlook.com>
2020-04-27 05:47:47 +0200

    analyzer v3

--- src/SyntaxTree/ExpressionNodes/ExpAnalyzer.cpp
+++ src/SyntaxTree/ExpressionNodes/ExpAnalyzer.cpp
@@ -1,25 +1,28 @@
 #include "../includes.h"
 #include "../../context/scope_context.h"
 
-
-
-bool AssignOprNode::analyze(ScopeContext* context, bool valueUsed) {
-    if (!(rhs->analyze(context, true) & lhs->analyze(context, false))) {
+bool AssignOprNode::analyze(ScopeContext *context, bool valueUsed)
+{
+    if (!(rhs->analyze(context, true) & lhs->analyze(context, false)))
+    {
         // Note that I used a bitwise AND to execute both lhs and rhs expressions
         return false;
     }
 
-
-    if (lhs->reference == NULL) {
+    if (lhs->reference == NULL)
+    {
         context->log("lvalue required as left operand of assignment", lhs->loc, LOG_ERROR);
         return false;
     }
-    if (lhs->reference && lhs->constant) {
+
+    if (lhs->reference && lhs->constant)
+    {
         context->log("assignment of read-only variable '" + lhs->reference->declaredHeader() + "'", lhs->loc, LOG_ERROR);
         return false;
     }
 
-    if(lhs->type != rhs->type) {
+    if (lhs->type != rhs->type)
+    {
         context->log("type missmatch: cannot cast " + rhs->exprTypeStr() + " to " + lhs->exprTypeStr(), lhs->loc, LOG_ERROR);
         return false;
     }
@@ -34,15 +37,17 @@ bool AssignOprNode::analyze(ScopeContext* context, bool valueUsed) {
     return true;
 }
 
-bool BinaryOprNode::analyze(ScopeContext* context, bool valueUsed) {
-    if (!(lhs->analyze(context, valueUsed) & rhs->analyze(context, valueUsed))) {
+bool BinaryOprNode::analyze(ScopeContext *context, bool valueUsed)
+{
+    if (!(lhs->analyze(context, valueUsed) & rhs->analyze(context, valueUsed)))
+    {
         // Note that I used a bitwise AND to execute both lhs and rhs expressions
         return false;
     }
 
-    if (lhs->type != rhs->type) {
-        context->log("invalid operands of types '" + lhs->exprTypeStr() + "' and '" + rhs->exprTypeStr() + "' to " +
-                     getOpr(), loc, LOG_ERROR);
+    if (lhs->type != rhs->type)
+    {
+        context->log("invalid operands of types '" + lhs->exprTypeStr() + "' and '" + rhs->exprTypeStr() + "' to " + getOpr(), loc, LOG_ERROR);
         return false;
     }
 
@@ -54,12 +59,15 @@ bool BinaryOprNode::analyze(ScopeContext* context, bool valueUsed) {
     return true;
 }
 
-bool UnaryOprNode::analyze(ScopeContext* context, bool valueUsed) {
-    if (!expr->analyze(context, valueUsed)) {
+bool UnaryOprNode::analyze(ScopeContext *context, bool valueUsed)
+{
+    if (!expr->analyze(context, valueUsed))
+    {
         return false;
     }
 
-    if (expr->type == DTYPE_FLOAT && Utils::isBitwiseOpr(opr)) {
+    if (expr->type == DTYPE_FLOAT && Utils::isBitwiseOpr(opr))
+    {
         context->log("invalid operand of type '" + expr->exprTypeStr() + "' to " + getOpr(), loc, LOG_ERROR);
         return false;
     }
@@ -72,23 +80,27 @@ bool UnaryOprNode::analyze(ScopeContext* context, bool valueUsed) {
     return true;
 }
 
-bool IdentifierNode::analyze(ScopeContext* context, bool valueUsed) {
-    DeclarationNode* ptr = context->getSymbol(name);
+bool IdentifierNode::analyze(ScopeContext *context, bool valueUsed)
+{
+    DeclarationNode *ptr = context->getSymbol(name);
 
-    if (ptr == NULL) {
+    if (ptr == NULL)
+    {
         context->log("'" + name + "' was not declared in this scope", loc, LOG_ERROR);
         return false;
     }
 
     reference = ptr;
     type = ptr->type->type;
-    constant = ((VarDeclarationNode*) ptr)->constant;
+    constant = ((VarDeclarationNode *)ptr)->constant;
     used = valueUsed;
-    if (used) {
+    if (used)
+    {
         reference->used++;
     }
 
-    if (used && !reference->initialized) {
+    if (used && !reference->initialized)
+    {
         context->log("variable or field '" + name + "' used without being initialized", loc, LOG_ERROR);
         return false;
     }
--- src/SyntaxTree/StatementNodes/StatementAnalyzer.cpp
+++ src/SyntaxTree/StatementNodes/StatementAnalyzer.cpp
@@ -1,51 +1,78 @@
 #include "../includes.h"
 #include "../../context/scope_context.h"
 
-
-bool ErrorNode::analyze(ScopeContext* context) {
+bool ErrorNode::analyze(ScopeContext *context)
+{
     context->log(what, loc, LOG_ERROR);
     return false;
 }
 
-bool BlockNode::analyze(ScopeContext* context) {
+bool BlockNode::analyze(ScopeContext *context)
+{
     bool ret = true;
     context->addScope(SCOPE_BLOCK, this);
-    for (int i = 0; i < statements.size(); ++i) {
+    for (int i = 0; i < statements.size(); ++i)
+    {
         ret &= statements[i]->analyze(context);
     }
     context->popScope();
     return ret;
 }
 
-bool VarDeclarationNode::analyze(ScopeContext* context) {
+bool VarDeclarationNode::analyze(ScopeContext *context)
+{
     bool ret = true;
-    if(value){
-        if(value->type != ident->type){
-            context->log("type missmatch: cannot cast " + value->exprTypeStr() + " to " + Utils::dtypeToStr(type->type), ident->loc, LOG_ERROR);
-            return false;
-        }
-    }
-    if (!context->declareSymbol(this)) {
+    if (!context->declareSymbol(this))
+    {
         context->log("'" + declaredHeader() + "' redeclared", ident->loc, LOG_ERROR);
         ret = false;
     }
-    if (value) {
+    if (value)
+    {
+        ValueNode *ptr = dynamic_cast<ValueNode *>(value);
+        if (ptr)
+        {
+            if (type->type != ptr->type)
+            {
+                context->log("type missmatch: cannot cast " + Utils::dtypeToStr(ptr->type) + " to " + Utils::dtypeToStr(type->type), ident->loc, LOG_ERROR);
+                return false;
+            }
+        }
+        IdentifierNode *ptr2 = dynamic_cast<IdentifierNode *>(value);
+        if (ptr2)
+        {
+            DeclarationNode *node = context->getSymbol(ptr2->name);
+            if (node)
+            {
+                if (type->type != node->type->type)
+                {
+                    context->log("type missmatch: cannot cast " + Utils::dtypeToStr(node->type->type) + " to " + Utils::dtypeToStr(type->type), ident->loc, LOG_ERROR);
+                    return false;
+                }
+            }
+        }
+    }
+    if (value)
+    {
         context->initializeVar = true;
         ret &= value->analyze(context, true);
         context->initializeVar = false;
     }
 
-    if (constant && value == NULL) {
+    if (constant && value == NULL)
+    {
         context->log("uninitialized const '" + ident->name + "'", ident->loc, LOG_ERROR);
         ret = false;
     }
     return ret;
 }
 
-bool MultiVarDeclarationNode::analyze(ScopeContext* context) {
+bool MultiVarDeclarationNode::analyze(ScopeContext *context)
+{
     int ret = true;
 
-    for (int i = 0; i < vars.size(); ++i) {
+    for (int i = 0; i < vars.size(); ++i)
+    {
         ret &= vars[i]->analyze(context);
     }
 
